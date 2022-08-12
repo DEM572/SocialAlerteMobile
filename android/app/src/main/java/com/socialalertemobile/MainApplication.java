9e8216a02c915b454a482d3d0ec6aed4895db0d5
Alassane <dema80273@gmail.com>
2022-08-12 11:44:26 +0200

    Developpement of SplashScreen

--- android/app/src/main/java/com/socialalertemobile/MainApplication.java
+++ android/app/src/main/java/com/socialalertemobile/MainApplication.java
@@ -13,7 +13,11 @@ import com.socialalertemobile.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.BV.LinearGradient.LinearGradientPackage;
+
 public class MainApplication extends Application implements ReactApplication {
+
+
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -26,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          packages.add(new LinearGradientPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
@@ -88,4 +93,5 @@ public class MainApplication extends Application implements ReactApplication {
       }
     }
   }
+
 }
